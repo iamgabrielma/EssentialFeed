22995081db2dd46457b1d29efed04afaa66297bf
iamgabrielma <gabrielmaldonado.art@gmail.com>
2023-04-02 10:59:17 +0700

    Make RemoteFeedLoader load requestedURL via DI

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,6 +9,9 @@ import XCTest
 
 class RemoteFeedLoader {
 
+    func load(client: HTTPCLient) {
+        client.requestedURL = URL(string: "foo")
+    }
 }
 
 class HTTPCLient {
@@ -20,8 +23,16 @@ final class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
         let client = HTTPCLient()
         _ = RemoteFeedLoader()
-        
-        
+
         XCTAssertNil(client.requestedURL)
+    }
+
+    func test_load_requestDataFromURL() {
+        let client = HTTPCLient()
+        let sut = RemoteFeedLoader()
+        
+        sut.load(client: client)
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 }
