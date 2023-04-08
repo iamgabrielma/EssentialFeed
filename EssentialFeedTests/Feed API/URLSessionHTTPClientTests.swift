451e61f3891246137b182969603d255ebf003ed8
iamgabrielma <gabrielmaldonado.art@gmail.com>
2023-04-08 10:33:04 +0700

    Fix override setup and teardown
    By overriding the class methods we get “API violation - multiple calls made to -[XCTestExpectation fulfill]” when running the whole set.

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -10,25 +10,26 @@ import EssentialFeed
 
 final class URLSessionHTTPClientTests: XCTestCase {
     
-    override class func setUp() {
+    override func setUp() {
         super.setUp()
         URLProtocolStub.startInterceptingRequests()
     }
     
-    override class func tearDown() {
+    override func tearDown() {
         super.tearDown()
         URLProtocolStub.stopInterceptingRequests()
     }
     
     func test_getFromURL_performsGETRequestWithURL() {
+        let url = anyURL()
         let exp = expectation(description: "Wait for completion")
 
         URLProtocolStub.observeRequests { request in
-            XCTAssertEqual(request.url, self.anyURL())
+            XCTAssertEqual(request.url, url)
             XCTAssertEqual(request.httpMethod, "GET")
             exp.fulfill()
         }
-        makeSUT().get(from: anyURL()) { _ in }
+        makeSUT().get(from: url) { _ in }
         
         wait(for: [exp], timeout: 1.0)
     }
