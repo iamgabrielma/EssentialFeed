c6640fc44b6e3f378ced7a3aef35d6f840b2b1e0
iamgabrielma <gabrielmaldonado.art@gmail.com>
2023-04-16 01:08:23 +0700

    Retrieving after inserting to empty cache delivers inserted values

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -10,16 +10,53 @@ import XCTest
 import EssentialFeed
 
 final class CodableFeedStore {
+    
+    private struct Cache: Codable {
+        let feed: [LocalFeedImage]
+        let timestamp: Date
+    }
+    
+    private let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+    
     func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
-        completion(.empty)
+        guard let data = try? Data(contentsOf: storeURL) else {
+            return completion(.empty)
+        }
+        let decoder = JSONDecoder()
+        let cache = try! decoder.decode(Cache.self, from: data)
+        completion(.found(feed: cache.feed, timestamp: cache.timestamp))
+        
+    }
+    
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
+        let encoder = JSONEncoder()
+        let encoded = try! encoder.encode(Cache(feed: feed, timestamp: timestamp))
+        try! encoded.write(to: storeURL)
+        completion(nil)
     }
 }
 
 final class CodableFeedStoreTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+        let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+        try? FileManager.default.removeItem(at: storeURL)
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+        try? FileManager.default.removeItem(at: storeURL)
+    }
+
     public func test_retrieve_deliversEmptyOnEmptyCache() {
         let sut = CodableFeedStore()
         
         let exp = expectation(description: "Wait for cache retrieval")
+
         sut.retrieve { result in
             switch result {
             case .empty:
@@ -29,7 +66,6 @@ final class CodableFeedStoreTests: XCTestCase {
             }
             exp.fulfill()
         }
-        
         wait(for: [exp], timeout: 1.0)
     }
     
@@ -37,6 +73,7 @@ final class CodableFeedStoreTests: XCTestCase {
         let sut = CodableFeedStore()
         
         let exp = expectation(description: "Wait for cache retrieval")
+        
         sut.retrieve { firstResult in
             sut.retrieve { secondResult in
                 switch(firstResult, secondResult) {
@@ -44,6 +81,29 @@ final class CodableFeedStoreTests: XCTestCase {
                     break
                 default:
                     XCTFail("Expected empty result, got \(firstResult) and \(secondResult) instead")
+                }
+                exp.fulfill()
+            }
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    public func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
+        let sut = CodableFeedStore()
+        let feed = uniqueImageFeed().local
+        let timestamp = Date()
+        let exp = expectation(description: "Wait for cache retrieval")
+
+        sut.insert(feed, timestamp: timestamp) { insertionError in
+            XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
+            
+            sut.retrieve { retrieveResult in
+                switch retrieveResult {
+                case let .found(retrievedFeed, retrievedTimestamp):
+                    XCTAssertEqual(retrievedFeed, feed)
+                    XCTAssertEqual(retrievedTimestamp, timestamp)
+                default:
+                    XCTFail("Expected found result with feed \(feed) and timestamp \(timestamp), got \(retrieveResult) instead")
                 }
             }
             exp.fulfill()
