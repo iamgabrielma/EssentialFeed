a64149dc1910fe89022d0c8f2c25ba174ad38429
iamgabrielma <gabrielmaldonado.art@gmail.com>
2023-04-16 19:19:27 +0700

    Make queue concurrent, and use barriers when performing side-effects to guarantee data consistency

--- EssentialFeed/Feed Cache/CodableFeedStore.swift
+++ EssentialFeed/Feed Cache/CodableFeedStore.swift
@@ -42,7 +42,7 @@ public final class CodableFeedStore: FeedStore {
         self.storeURL = storeURL
     }
     
-    private let queue = DispatchQueue(label: "\(CodableFeedStore.self)Queue", qos: .userInitiated)
+    private let queue = DispatchQueue(label: "\(CodableFeedStore.self)Queue", qos: .userInitiated, attributes: .concurrent)
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
         let storeURL = self.storeURL
@@ -64,7 +64,7 @@ public final class CodableFeedStore: FeedStore {
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         let storeURL = self.storeURL
 
-        queue.async {
+        queue.async(flags: .barrier) {
             do {
                 let encoder = JSONEncoder()
                 let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
@@ -80,7 +80,7 @@ public final class CodableFeedStore: FeedStore {
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         let storeURL = self.storeURL
         
-        queue.async {
+        queue.async(flags: .barrier) {
             guard FileManager.default.fileExists(atPath: storeURL.path) else {
                 return completion(nil)
             }
