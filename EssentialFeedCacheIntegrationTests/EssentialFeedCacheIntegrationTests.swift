374d629b1bff316345f0bea9f3a3cd3d4a6a4435
iamgabrielma <gabrielmaldonado.art@gmail.com>
2023-04-17 18:49:16 +0700

    Add integration tests target

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Gabriel Maldonado Almendra on 17/4/23.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+
+}
