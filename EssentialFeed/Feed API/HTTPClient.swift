161ba0d2d94164c419621359614aa27e6a552c97
iamgabrielma <gabrielmaldonado.art@gmail.com>
2023-04-18 21:45:52 +0700

    Replace custom `HTTPClientResult` with standard Result type

--- EssentialFeed/Feed API/HTTPClient.swift
+++ EssentialFeed/Feed API/HTTPClient.swift
@@ -8,12 +8,9 @@
 import Foundation
 
 public protocol HTTPClient {
+    typealias Result = Swift.Result<(Data,HTTPURLResponse), Error>
+
     /// The completion handler can be invoked in any thread.
     /// Clients are responsible to dispatch to appropriate threads, if needed.
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
-}
-
-public enum HTTPClientResult {
-    case success(Data, HTTPURLResponse)
-    case failure(Error)
+    func get(from url: URL, completion: @escaping (Result) -> Void)
 }
