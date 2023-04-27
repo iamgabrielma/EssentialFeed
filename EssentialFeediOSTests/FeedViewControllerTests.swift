e66cb105561d267ec1e9264a78205b5ae5747f36
iamgabrielma <gabrielmaldonado.art@gmail.com>
2023-04-27 19:43:07 +0700

    Extract `cancelImageDataLoad` method into a task that can be cancelled

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -231,11 +231,20 @@ private extension FeedViewControllerTests {
 
         // MARK: - FeedImageDataLoader conformance
         //
+        private struct TaskSpy: FeedImageDataLoaderTask {
+            let cancelCallback: () -> Void
+            func cancel() {
+                cancelCallback()
+            }
+        }
         private(set) var loadedImageURLs = [URL]()
         private(set) var cancelledImageURLs = [URL]()
 
-        func loadImageData(from url: URL) {
+        func loadImageData(from url: URL) -> EssentialFeediOS.FeedImageDataLoaderTask {
             loadedImageURLs.append(url)
+            return TaskSpy { [weak self] in
+                self?.cancelledImageURLs.append(url)
+            }
         }
         
         func cancelImageDataLoad(from url: URL) {
