af0e82cd619b5e7a33ba39c72081fc89ce60d28e
iamgabrielma <gabrielmaldonado.art@gmail.com>
2023-04-27 21:28:19 +0700

    Feed image view loading indicator is visible while loading image

--- EssentialFeediOS/FeedImageCell.swift
+++ EssentialFeediOS/FeedImageCell.swift
@@ -9,6 +9,7 @@ import UIKit
 
 final public class FeedImageCell: UITableViewCell {
     public var locationContainer = UIView()
+    public var feedImageContainer = UIView()
     public var descriptionLabel = UILabel()
     public var locationLabel = UILabel()
 }
--- EssentialFeediOS/FeedViewController.swift
+++ EssentialFeediOS/FeedViewController.swift
@@ -13,7 +13,8 @@ public protocol FeedImageDataLoaderTask {
 }
 
 public protocol FeedImageDataLoader {
-    func loadImageData(from url: URL) -> FeedImageDataLoaderTask
+    typealias Result = Swift.Result<Data, Error>
+    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
 }
 
 final public class FeedViewController: UITableViewController {
@@ -58,8 +59,11 @@ final public class FeedViewController: UITableViewController {
         cell.locationContainer.isHidden = (cellModel.location == nil)
         cell.descriptionLabel.text = cellModel.description
         cell.locationLabel.text = cellModel.location
+        cell.feedImageContainer.startShimmering()
         
-        tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url)
+        tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url) { [weak cell] result in
+            cell?.feedImageContainer.stopShimmering()
+        }
         
         return cell
     }
