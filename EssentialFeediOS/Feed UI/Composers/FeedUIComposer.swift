df7a08f2f2fe0e961e3ba0da3743508a0ad66586
iamgabrielma <gabrielmaldonado.art@gmail.com>
2023-05-09 22:04:53 +0700

    Extract FeedVC creation and config to factory method

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -12,15 +12,13 @@ final public class FeedUIComposer {
     private init() {}
 
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
-        
         let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader)
         
-        let bundle = Bundle(for: FeedViewController.self)
-        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
-        let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
-        feedController.delegate = presentationAdapter
-        feedController.title = FeedPresenter.title
-        
+        let feedController = FeedViewController.makeWith(
+            delegate: presentationAdapter,
+            title: FeedPresenter.title
+        )
+                
         presentationAdapter.presenter = FeedPresenter(
             feedView: FeedViewAdapter(controller: feedController, imageLoader: imageLoader),
             feedLoadingView: WeakRefVirtualProxy(feedController)
@@ -30,7 +28,19 @@ final public class FeedUIComposer {
     }
 }
 
-final private class WeakRefVirtualProxy<T: AnyObject> {
+private extension FeedViewController {
+    static func makeWith(delegate: FeedViewControllerDelegate, title: String ) -> FeedViewController {
+        let bundle = Bundle(for: FeedViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
+        feedController.delegate = delegate
+        feedController.title = title
+        
+        return feedController
+    }
+}
+
+private final class WeakRefVirtualProxy<T: AnyObject> {
     private weak var object: T?
     
     init(_ object: T? = nil) {
